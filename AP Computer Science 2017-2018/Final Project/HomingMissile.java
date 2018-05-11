51be0da35cc88db2f7b4d7f19be5c72c9fe649c9
Albert Lin <Albertkuilin@gmail.com>
2018-05-11 01:03:17 -0700

    more progress

--- AP Computer Science 2017-2018/Final Project/HomingMissile.java
+++ AP Computer Science 2017-2018/Final Project/HomingMissile.java
@@ -1,19 +1,11 @@
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.geom.Ellipse2D;
 
-public class HomingMissile extends Ellipse2D.Double {
+
+public class HomingMissile extends MovableGamePiece {
 	private final static double size = 10;
-	private Vector velocity;
 	
 	public HomingMissile(int xLoc, int yLoc, double angle, double speed) {
-		super(xLoc, yLoc, size, size);
-		velocity = new Vector(angle, speed);
+		super(xLoc, yLoc, angle, speed, size, size);
 	}
 	
-	public void moveTick() {
-		x+=velocity.getXMagnitude();
-		y+=velocity.getYMagnitude();
-	}
 
 }
--- AP Computer Science 2017-2018/Final Project/LaserBeam.java
+++ AP Computer Science 2017-2018/Final Project/LaserBeam.java
@@ -1,19 +1,10 @@
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.geom.Ellipse2D;
 
-public class LaserBeam extends Ellipse2D.Double {
-	private final static double size = 10;
-	private Vector velocity;
+public class LaserBeam extends MovableGamePiece {
+	private final static double width = 10000, height = 20;
 	
 	public LaserBeam(int xLoc, int yLoc, double angle, double speed) {
-		super(xLoc, yLoc, size, size);
-		velocity = new Vector(angle, speed);
-	}
-	
-	public void moveTick() {
-		x+=velocity.getXMagnitude();
-		y+=velocity.getYMagnitude();
+		super(xLoc, yLoc, angle, speed, width, height);
 	}
 
 }
